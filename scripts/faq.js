9167064589cf912a72715b980ee757d1dbb882f1
Karin <Kaykins@users.noreply.github.com>
2015-11-07 21:32:41 +0100

    Update faq.js

--- scripts/faq.js
+++ scripts/faq.js
@@ -7,21 +7,6 @@ $(document).ready(function(){
 		$(this).parent(".item").toggleClass("open");
    
    });
-
-   $(document).ready(function(){
-      $('body').append('<div id="toTop" class="btn"> Back to top</div>');
-    	$(window).scroll(function () {
-			if ($(this).scrollTop() != 0) {
-				$('#toTop').fadeIn();
-			} else {
-				$('#toTop').fadeOut();
-			}
-		}); 
-    $('#toTop').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 600);
-        return false;
-    });
-});
    
 });
 
